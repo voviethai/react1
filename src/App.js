8947f14a7141cb8df1f1dd14870e8fc2600ad575
Vo Viet Hai <voviethai2012@gmail.com>
2021-07-29 22:21:08 +0700

    update nunber

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Nice
+          Learn React Nice 123
         </a>
       </header>
     </div>
